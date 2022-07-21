01320fe1c420cf858ce9a463bc8c79692aeb3722
kerellka <kerellkay@gmail.com>
2022-07-21 12:34:43 +0300

    remarks

--- src/main/kotlin/crudAppilcation/PersonDataStorage.kt
+++ src/main/kotlin/crudAppilcation/PersonDataStorage.kt
@@ -1,3 +1,6 @@
 package crudAppilcation
 
+//FIXME лучше такие небольшие классы определять рядом с местом, где они используются
+// - например в PersonService
+// - все таки это на Java, где каждый класс надо определять в отдельном файле
 data class Person(val id: Int, val name: String, val lastName: String)
